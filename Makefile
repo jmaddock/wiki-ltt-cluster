acb76551d5cfddc8c3a31d50eb4755fd26d320bc
Jim Maddock <skimaddster@gmail.com>
2021-02-02 08:49:06 -0700

    implimented threaded preprocessor for handling multiple files

--- Makefile
+++ Makefile
@@ -1,19 +1,21 @@
 all: vectorize cluster
 
 # shouldn't hardcode input dir
+# remove debug flag
 vectorize: word2vec/enwiki-20200501-learned_vectors.50_cell.10k.kv
 	python ./wiki-ltt-cluster/clean_and_vectorize.py \
 		~/research_data/wmf_knowledge_graph/raw_xml_dumps/enwiki/ \
 		./datasets/vectorized \
 		./word2vec/enwiki-20200501-learned_vectors.50_cell.10k.kv \
-		--debug 100
+		-d 100 \
+		-w 4
 
 # need to update this with reasonable value of k
 cluster:
 	python ./wiki-ltt-cluster/cluster.py \
 		./datasets/vectorized \
 		datasets/clustered/enwiki_clustered_26-01-2021.json \
-		-k 2 5 1 
+		-k 2 5 1
 
 word2vec/enwiki-20200501-learned_vectors.50_cell.10k.kv:
 	wget https://analytics.wikimedia.org/datasets/archive/public-datasets/all/ores/topic/vectors/enwiki-20200501-learned_vectors.50_cell.10k.kv -qO- > $@
--- wiki-ltt-cluster/clean_and_vectorize.py
+++ wiki-ltt-cluster/clean_and_vectorize.py
@@ -8,6 +8,8 @@ import argparse
 import logging
 import functools
 
+from concurrent.futures import ThreadPoolExecutor
+
 from revscoring.features import wikitext
 from revscoring.datasources.meta import mappers, vectorizers
 from revscoring.datasources import revision_oriented
@@ -18,9 +20,13 @@ import mwxml
 
 class CleanAndVectorize(object):
 
-    def __init__(self, en_kvs_path):
+    def __init__(self, en_kvs_path, **kwargs):
         self.tokenizer = mappers.lower_case(wikitext.revision.datasources.words)
         self.vectorizer = self.load_vectorizer(en_kvs_path)
+        self.workers = kwargs.get('workers',1)
+        self.save_text = kwargs.get('save_text',False)
+        self.save_tokens = kwargs.get('save_tokens', False)
+        self.debug = kwargs.get('debug', False)
 
     def load_vectorizer(self, enwiki_kvs_path):
         enwiki_kvs = vectorizers.word2vec.load_gensim_kv(
@@ -43,50 +49,54 @@ class CleanAndVectorize(object):
 
         return w2v
 
-    def get_dumps_from_dir(self,indir):
-        file_list = [f for f in os.listdir(indir)
-                     if f.find('enwiki-latest-pages-articles') >= 0
-                     and f.find('multistream') < 0
-                     and f.find('rss') < 0
-                     and f.find('enwiki-latest-pages-articles.xml.bz2') < 0]
+    def vectorize(self, filestream):
+        dump = mwxml.Dump.from_file(filestream)
+        for i, page in enumerate(dump):
+            for rev in page:
+                observation = {
+                    'title':page.title,
+                    'page_id':page.id,
+                    'rev_id':rev.id,
+                    'redirect':page.redirect
+                }
 
-        return file_list
+                cache = {}
 
-    def get_dumps_from_web(self):
-        pass
+                cache[revision_oriented.revision.text] = rev.text
+                tokenized_text = solve(self.tokenizer, cache=cache, context=None)
 
-    def process_dump(self, filepath, save_text=False, save_tokens=False, debug=False):
-        with bz2.open(filepath) as filestream:
-            dump = mwxml.Dump.from_file(filestream)
-            for i, page in enumerate(dump):
-                for rev in page:
-                    observation = {
-                        'title':page.title,
-                        'page_id':page.id,
-                        'rev_id':rev.id,
-                        'redirect':page.redirect
-                    }
+                cache[self.tokenizer] = tokenized_text
+                observation['feature_vector'] = solve(self.vectorizer, cache=cache, context=None)
 
-                    cache = {}
+                if self.save_text:
+                    observation['text'] = rev.text
 
-                    cache[revision_oriented.revision.text] = rev.text
-                    tokenized_text = solve(self.tokenizer, cache=cache, context=None)
+                if self.save_tokens:
+                    observation['tokenized_text'] = tokenized_text
 
-                    cache[self.tokenizer] = tokenized_text
-                    observation['feature_vector'] = solve(self.vectorizer, cache=cache, context=None)
+                yield observation
 
-                    if save_text:
-                        observation['text'] = rev.text
+            if self.debug:
+                if i == self.debug:
+                    break
 
-                    if save_tokens:
-                        observation['tokenized_text'] = tokenized_text
+    def _process_dump(self,infile_path,outfile_path):
+        with bz2.open(infile_path) as filestream:
+            with open(outfile_path,'w') as outfile:
+                outfile.write('[')
+                for i, obs in enumerate(self.vectorize(filestream)):
+                    if i > 0:
+                        outfile.write(',')
+                        if i % 10 == 0:
+                            print('{0}, {1}, {2}'.format(infile_path,outfile_path,i))
+                    json.dump(obs,outfile)
+                outfile.write(']')
 
-                    yield observation
-
-                if debug:
-
-                    if i == debug:
-                        break
+    def process(self,files_to_process,files_to_write):
+        with ThreadPoolExecutor(max_workers=self.workers) as executor:
+            executor.map(self._process_dump,
+                         files_to_process,
+                         files_to_write)
 
 def main():
     parser = argparse.ArgumentParser(description='Convert .bz2 compressed xml dumps of Wikipedia articles to feature vectors.')
@@ -96,14 +106,11 @@ def main():
                         help='a directory to write feature vectors')
     parser.add_argument('embedding_file',
                         help='a kvs embedding layer file')
-    parser.add_argument('-v', '--verbose',
-                        action='store_true')
-    parser.add_argument('-d', '--debug',
-                        type=int)
-    parser.add_argument('--save_text',
-                        action='store_true')
-    parser.add_argument('--save_tokens',
-                        action='store_true')
+    parser.add_argument('-w', '--workers', type=int, default=1)
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-d', '--debug', type=int, default=False)
+    parser.add_argument('--save_text', action='store_true')
+    parser.add_argument('--save_tokens', action='store_true')
     args = parser.parse_args()
 
     logger = logging.getLogger(__name__)
@@ -119,23 +126,22 @@ def main():
     else:
         logger.setLevel(logging.INFO)
 
-    cv = CleanAndVectorize(args.embedding_file)
+    cv = CleanAndVectorize(en_kvs_path=args.embedding_file,
+                           workers=args.workers,
+                           debug=args.debug,
+                           save_text=args.save_text,
+                           save_tokens=args.save_tokens)
 
-    files_to_process = cv.get_dumps_from_dir(args.indir)
-    for infile in files_to_process:
-        infile_path = os.path.join(args.indir,infile)
-        outfile_name = '{0}.json'.format(infile.split('.')[0])
-        outfile_path = os.path.join(args.outdir,outfile_name)
-        with open(outfile_path,'w') as outfile:
-            outfile.write('[')
-            for i, obs in enumerate(cv.process_dump(infile_path,
-                                       save_text=args.save_text,
-                                       save_tokens=args.save_tokens,
-                                       debug=args.debug)):
-                if i > 0:
-                    outfile.write(',')
-                json.dump(obs,outfile)
-            outfile.write(']')
+    files_to_process = [os.path.join(args.indir,f) for f in os.listdir(args.indir)
+                        if f.find('enwiki-latest-pages-articles') >= 0
+                        and f.find('multistream') < 0
+                        and f.find('rss') < 0
+                        and f.find('enwiki-latest-pages-articles.xml.bz2') < 0]
+
+    files_to_write = [os.path.join(args.outdir,'{0}.json'.format(os.path.basename(f).rstrip('.bz2'))) for f in files_to_process]
+
+    cv.process(files_to_process,files_to_write)
+
 
 if __name__ == "__main__":
     main()
